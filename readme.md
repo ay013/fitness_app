db2224b6974fb88bcc1d95d00a8b7a1e0c75832e
ay013 <aaravyadav8141@gmail.com>
2026-06-20 12:05:07 +0530

    commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+  cookie-parser ->The cookie-parser npm package functions as a middleware for Express.js that extracts the raw Cookie header from incoming HTTP requests, parses it into a usable JavaScript object, and exposes it via req.cookies
+  cors->CORS is a Node.js middleware for Express/Connect that sets CORS response headers. These headers tell browsers which origins can read responses from your server
---
+++ src/app.js
@@ -0,0 +1,25 @@
+// src / app.js: This is where you configure Express.You will initialize const app = express(), set up your middlewares (like CORS, cookie - parser, or JSON body limits), and import/mount your route files here.
+import express from "express"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+const app = express()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+app.use(express.json({ limit: "16kb" }))// we have limited the json to 16 kb
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+app.use(cookieParser())
+//routes   import 
+
+
+// import UserRouter from './routes/user.routes.js'
+// // routes declaration 
+// app.use("/api/v1/users", UserRouter) 
+
+
+
+app.get("/ping", (req, res) => res.send("Pong!"));
+export {app}
